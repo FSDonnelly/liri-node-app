dc2fd01fe53de9ff94a92039f7ce88a631337ca1
FSDonnelly <donnelly_frank@yahoo.com>
2018-12-05 18:56:18 -0800

    saved api keys and urls

--- keys.js
+++ keys.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 console.log('this is loaded');
-
+// Get Spotify key and secret from .env
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+// Bands in Town Artist Events url
+var bandUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
+// Omdb api url
+var movieUrl = `http://www.omdbapi.com/?t=${userText}&y=&plot=short&apikey=trilogy`
+
+
--- liri.js
+++ liri.js
@@ -1,3 +1,15 @@
 require("dotenv").config();
-
+// access spotify
 var spotify = new Spotify(keys.spotify);
+// including the fs or filesystem package
+var fs = require("fs");
+
+var axios = require("axios");
+// take in a user input
+var userInput = process.argv[2];
+var userText = "";
+// Bands in Town Artist Events url
+var bandUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`;
+// Omdb api url
+var movieUrl = `http://www.omdbapi.com/?t=${userText}&y=&plot=short&apikey=trilogy`;
+
